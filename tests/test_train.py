3367ef5c42765a60865afc9960a1aa7bd6b4a3be
2022AC05547 <2022AC05547@wilp.bits-pilani.ac.in>
2024-08-01 22:17:02 +0530

    Update test_train.py

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,31 +1,28 @@
-# tests/test_train.py
-
-import sys
 import os
+import sys
 import pytest
 from train import train
 
 # Redirect stderr to avoid fileno issues
 sys.stderr = open(os.devnull, 'w')
+
 # Add src directory to sys.path
-sys.path.insert(0, os.path.abspath
-                (os.path.join(os.path.dirname(__file__), '../src')))
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src'))
+)
 
 def test_train():
-
-  
+    """Test the training function."""
     # Ensure the model directory does not exist before training
-
-    if os.path.exists("model/logistic_regression_model.pkl"):
-        os.remove("model/logistic_regression_model.pkl")
+    model_path = "model/logistic_regression_model.pkl"
+    if os.path.exists(model_path):
+        os.remove(model_path)
 
     # Run the training function
     train()
 
     # Check that the model file is created
-    assert os.path.exists(
-    "model/logistic_regression_model.pkl"), "Model file not created"
-
+    assert os.path.exists(model_path), "Model file not created"
 
 if __name__ == "__main__":
     pytest.main(["-v", "tests/test_train.py"])
