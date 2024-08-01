3ec6ea9a69cc82229c59a4884d3a821256ac6763
2022AC05547 <2022AC05547@wilp.bits-pilani.ac.in>
2024-08-01 22:09:32 +0530

    Update test.py

--- src/test.py
+++ src/test.py
@@ -24,7 +24,8 @@ def test_model():
     X, y = iris.data, iris.target
 
     # Split the dataset into training and testing sets
-    _, X_test, _, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    _, X_test, _, y_test = train_test_split(
+                    X, y, test_size=0.2, random_state=42)
 
     # Make predictions with the model
     y_pred = model.predict(X_test)
